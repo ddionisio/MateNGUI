851af3a02d381e4fa280512ff40b23a07bb25bde
David Dionisio <garapata@gmail.com>
2014-04-02 17:30:35 -0700

    more scripts from MateUnity

---
+++ Editor/NGUIModalInputInspector.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+
+[CustomEditor(typeof(NGUIModalInput))]
+public class UIModalInputNGUIInspector : Editor {
+    public override void OnInspectorGUI() {
+        GUI.changed = false;
+
+		NGUIModalInput obj = target as NGUIModalInput;
+
+        obj.player = EditorGUILayout.IntField("Player", obj.player);
+
+        obj.axisX = M8.Editor.InputMapper.GUISelectInputAction("Axis X", obj.axisX);
+        obj.axisY = M8.Editor.InputMapper.GUISelectInputAction("Axis Y", obj.axisY);
+
+        obj.axisDelay = EditorGUILayout.FloatField("Axis Delay", obj.axisDelay);
+        obj.axisThreshold = EditorGUILayout.FloatField("Axis Threshold", obj.axisThreshold);
+
+        obj.enter = M8.Editor.InputMapper.GUISelectInputAction("Enter", obj.enter);
+        obj.enterAlt = M8.Editor.InputMapper.GUISelectInputAction("Enter Alt", obj.enterAlt);
+        obj.cancel = M8.Editor.InputMapper.GUISelectInputAction("Cancel", obj.cancel);
+        obj.cancelAlt = M8.Editor.InputMapper.GUISelectInputAction("Cancel Alt", obj.cancelAlt);
+
+        if(GUI.changed)
+            EditorUtility.SetDirty(target);
+    }
+}
---
+++ Script/NGUIModalActiveSelect.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Set given 'select' to UICamera.selectObject upon modal active.
+/// </summary>
+[AddComponentMenu("M8/NGUI/ModalActiveSelect")]
+public class NGUIModalActiveSelect : MonoBehaviour {
+
+    public GameObject select;
+
+    private UIController mController;
+
+    void OnDestroy() {
+        if(mController != null) {
+            mController.onActiveCallback -= UIActive;
+        }
+    }
+
+    void Awake() {
+        mController = GetComponent<UIController>();
+        if(mController != null) {
+            mController.onActiveCallback += UIActive;
+        }
+    }
+
+    void UIActive(bool active) {
+        if(active && select.activeInHierarchy) {
+            UICamera.selectedObject = select;
+        }
+    }
+}
